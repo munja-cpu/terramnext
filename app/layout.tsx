d8856e58df065007ef1d1447b29a1aefdc80cfef
munja-cpu <munjasjovan@gmail.com>
2026-04-05 00:05:41 +0200

    optimizovan SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,41 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Inter, Playfair_Display } from "next/font/google";
 
+export const metadata = {
+  title: "Terram | Terarijumi",
+  description: "Ručno izrađeni terarijumi za dom i kancelariju",
+
+  keywords: [
+    "terarijum",
+    "terarijumi",
+    "biljke u staklu",
+    "dekoracija enterijera",
+    "office biljke",
+    "kancelarijske biljke",
+    "zeleni dekor",
+  ],
+  openGraph: {
+    title: "Terram",
+    description: "Priroda u vašem prostoru",
+    url: "https://tvoj-sajt.vercel.app",
+    siteName: "Terram",
+    images: [
+      {
+        url: "https://terramnext.vercel.app/hero.webp",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "sr_RS",
+    type: "website",
+  },
+};
+
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
 });
+
 
 const playfair = Playfair_Display({
   subsets: ["latin"],
