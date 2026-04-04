1db68b2a3fcd2c2699ca2938190ce88e522d37fa
munja-cpu <munjasjovan@gmail.com>
2026-04-05 00:36:09 +0200

    speed check 0.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,28 +3,19 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Inter, Playfair_Display } from "next/font/google";
 
-// export const metadata = {
-//   title: "Terram | Terarijumi",
-//   description: "Ručno izrađeni terarijumi za dom i kancelariju",
+export const metadata = {
+  title: "Terram | Terarijumi",
+  description: "Ručno izrađeni terarijumi za dom i kancelariju",
 
-//   keywords: [
-//     "terarijum",
-//     "terarijumi",
-//     "biljke u staklu",
-//     "dekoracija enterijera",
-//     "office biljke",
-//     "kancelarijske biljke",
-//     "zeleni dekor",
-//   ],
-//   openGraph: {
-//     title: "Terram",
-//     description: "Priroda u vašem prostoru",
-//     url: "https://terramnext.vercel.app",
-//     siteName: "Terram",
-//     locale: "sr_RS",
-//     type: "website",
-//   },
-// };
+  openGraph: {
+    title: "Terram",
+    description: "Priroda u vašem prostoru",
+    url: "https://terramnext.vercel.app",
+    siteName: "Terram",
+    locale: "sr_RS",
+    type: "website",
+  },
+};
 
 const inter = Inter({
   subsets: ["latin"],
